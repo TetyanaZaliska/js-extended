3853bc51c0b3266ab5a07377e964234298606895
Tanya <tetyana.a.zaliska@gmail.com>
2021-11-01 13:43:53 +0200

    exlesson 4 - add post request to big server

--- js/script.js
+++ js/script.js
@@ -265,6 +265,60 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Forms
 
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Loading',
+        success: 'Done! We will contact to you as soon as possible',
+        failure: 'Something do wrong'
+    };
+
+    forms.forEach(item => postFrom(item));
+
+    function postFrom(form){
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest(); 
+            request.open('POST', 'server.php');
+
+            const formData = new FormData(form);
+            
+            const object = {};
+            formData.forEach(function(value, key) {
+                object[key] = value;
+            });
+            
+            const json = JSON.stringify(object);
+
+            // для форм-даты заголовок не нужен
+            // // request.setRequestHeader('Content-type', 'multipart/form-data; charset=utf-8');
+            // request.send(formData); 
+            
+            // для json даты заголовок нужен
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send(json); 
+
+            request.addEventListener('load', function () {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 4000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+
+        });
+    }
     
 
 });
