7813429ac182bb862b7da44bf0a17222920461df
Tanya <tetyana.a.zaliska@gmail.com>
2021-11-02 19:04:08 +0200

    exlesson 11 - added library axios

--- js/script.js
+++ js/script.js
@@ -222,29 +222,37 @@ document.addEventListener('DOMContentLoaded', () => {
     //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
     //         });
     //     });
-    getResource('http://localhost:3000/menu')
-        .then(data => createCard(data));
-
-    function createCard(data) {
-        data.forEach(({img, altimg, title, descr, price}) => {
-            const usdRate = 27;
-            const element = document.createElement('div');
-            element.classList.add('menu__item');
-
-            element.innerHTML = ` 
-                                <img src=${img} alt=${altimg}>
-                                <h3 class="menu__item-subtitle">${title}</h3>
-                                <div class="menu__item-descr">${descr}</div>
-                                <div class="menu__item-divider"></div>
-                                <div class="menu__item-price">
-                                    <div class="menu__item-cost">Цена:</div>
-                                    <div class="menu__item-total"><span>${price*usdRate}</span> грн/день</div>
-                                </div>
-                            `;
-            document.querySelector('.menu .container').append(element);
-                
+    
+    axios.get('http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
         });
-    }
+
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => createCard(data));
+
+    // function createCard(data) {
+    //     data.forEach(({img, altimg, title, descr, price}) => {
+    //         const usdRate = 27;
+    //         const element = document.createElement('div');
+    //         element.classList.add('menu__item');
+
+    //         element.innerHTML = ` 
+    //                             <img src=${img} alt=${altimg}>
+    //                             <h3 class="menu__item-subtitle">${title}</h3>
+    //                             <div class="menu__item-descr">${descr}</div>
+    //                             <div class="menu__item-divider"></div>
+    //                             <div class="menu__item-price">
+    //                                 <div class="menu__item-cost">Цена:</div>
+    //                                 <div class="menu__item-total"><span>${price*usdRate}</span> грн/день</div>
+    //                             </div>
+    //                         `;
+    //         document.querySelector('.menu .container').append(element);
+                
+    //     });
+    // }
  
     // Forms
 
