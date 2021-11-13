7b7cbcefb9d6bd4e3581c109265a367eb1bfa7a5
Tanya <tetyana.a.zaliska@gmail.com>
2021-11-14 00:01:57 +0200

    exlesson 26 - try catch finally

---
+++ testlessons/sctrycatchfin.js
@@ -0,0 +1,16 @@
+'use strict';
+
+try {
+    console.log('Normal');
+    console.log(a);
+    console.log('some other try inctructions');
+} catch(err) {
+    console.log("Error");
+    console.log(err.name);
+    console.log(err.message);
+    console.log(err.stack);
+} finally {
+    console.log('Run after try or catch instructions');
+}
+
+console.log('go to other script instruction');
