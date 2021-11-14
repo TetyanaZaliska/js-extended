0a508d815b439bdad2302f74145457b02ff3fc87
Tanya <tetyana.a.zaliska@gmail.com>
2021-11-14 14:32:14 +0200

    exlesson 27 - webpack

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+require('es6-promise').polyfill();
 
 import tabs from './modules/tabs';
 import timer from './modules/timer';
--- webpack.config.js
+++ webpack.config.js
@@ -14,4 +14,22 @@ module.exports = {
   devtool: "source-map",
 
   module: {}
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.m?js$/,
+  //       exclude: /(node_modules|bower_components)/,
+  //       use: {
+  //         loader: 'babel-loader',
+  //         options: {
+  //           presets: [['@babel/preset-env', {
+  //               debug: false, //true,
+  //               corejs: 3,
+  //               useBuiltIns: "usage"
+  //           }]]
+  //         }
+  //       }
+  //     }
+  //   ]
+  // }
 };
